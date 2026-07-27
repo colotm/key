a48f37331287836b97d8fb5628591b7901f399dd
Titima <colonruizanabel@gmail.com>
2026-07-26 22:31:11 -0400

    Add language names to enablinglanguage index

---
+++ release/el/enablinglanguage/index.js
@@ -0,0 +1,81 @@
+Abua
+Bokyi
+Bura-Pabir
+Cook Islands Maori
+Dayi
+Degema
+Dhangu-Djangu
+Dhuwal
+Dhuwaya
+Dinka (General)
+Djambarrpuyngu
+Djinang
+Djinba
+Ebira
+Edo
+Efik
+Ekajuk
+Ekpeye
+Eleme
+Engenni
+Esan
+Etsako
+Ezaa
+Fijian
+Fulah
+Fulfulde
+Gbagyi
+Ghotuo
+Gokana
+Gude
+Gumatj
+Gupapuyngu
+Harari
+Hausa
+Hawaiian
+Hohumono
+Idoma
+Ifè
+Igala
+Igbo
+Igede
+Ijo, Southeast
+Ikwere
+Ikwo
+Isekiri
+Isoko
+Izii
+Izon
+Jju
+Kamwe
+Kanuri
+Khana
+Kirike
+Lokaa
+Maori
+Marghi Central
+Mgbolizhia
+Niuean
+Nheastern Dinka
+Nthwestern Dinka
+Nuer
+Nupe-Nupe-Tako
+Obolo
+Odual
+Ogbah
+Ogbia
+Osmanya
+Persian
+Ritharrngu
+Samoan
+South Central Dinka
+Southeastern Dinka
+Tahitian
+Tangale
+Tera
+Tiv
+Tongan
+Tyap
+Urhobo
+Yan-nhangu
+Yoruba
